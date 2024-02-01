f408f98c89d70ea876c3922d16d9570081bb22ec
sebib21 <beusebiu12@gmail.com>
2024-02-01 18:46:31 +0200

    001 - Set initial SpringBoot project

---
+++ caloriecounter/src/main/java/com/sebaba/caloriecounter/CaloriecounterApplication.java
@@ -0,0 +1,13 @@
+package com.sebaba.caloriecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriecounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriecounterApplication.class, args);
+	}
+
+}
---
+++ caloriecounter/src/test/java/com/sebaba/caloriecounter/CaloriecounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebaba.caloriecounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriecounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
